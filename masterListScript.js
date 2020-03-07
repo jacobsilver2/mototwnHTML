8358aa0e5f1c523bf6eba0275ad828097b1591fe
Jacob Silver <jacobsilver2@mac.com>
2020-03-07 18:24:46 -0500

    moving closer with a real dataset

--- masterListScript.js
+++ masterListScript.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
     const publishedDate = $(song).find(".published-date");
     const altTitle = $(song).find(".alt-title");
     const instrumental = $(song).find(".instrumental");
+    const fromFilm = $(song).find(".from-film");
+    const tributeTo = $(song).find(".tribute-to");
 
     composer[0] && Object.assign(titleObj, { composer: composer[0].innerText });
     title[0] && Object.assign(titleObj, { title: title[0].innerText });
@@ -21,17 +23,48 @@ $(document).ready(function() {
     altTitle[0] && Object.assign(titleObj, { altTitle: altTitle[0].innerText });
     instrumental[0] &&
       Object.assign(titleObj, { instrumental: instrumental[0].innerText });
+    fromFilm[0] && Object.assign(titleObj, { fromFilm: fromFilm[0].innerText });
+    tributeTo[0] &&
+      Object.assign(titleObj, { tributeTo: tributeTo[0].innerText });
 
     songObj.title = titleObj;
 
     $(song)
       .find(".artist")
       .each((key, artist) => {
+        const albumsArr = [];
+        const recordingInfoObj = {};
         const artistName = $(artist).find(".artist-name");
+        const producer = $(artist).find(".producer");
+        const recordingLocation = $(artist).find(".recording-location");
+        const recordingCompleted = $(artist).find(".recording-completed");
+
         artistName[0] &&
           Object.assign(artistObj, { artistName: artistName[0].innerText });
-
+        producer[0] &&
+          Object.assign(recordingInfoObj, { producer: producer[0].innerText });
+        recordingLocation[0] &&
+          Object.assign(recordingInfoObj, {
+            location: recordingLocation[0].innerText
+          });
+        recordingCompleted[0] &&
+          Object.assign(recordingInfoObj, {
+            completed: recordingCompleted[0].innerText
+          });
+        artistObj.recordingInfo = recordingInfoObj;
         songObj.artist = artistObj;
+
+        $(artist)
+          .find(".album")
+          .each((key, album) => {
+            const albumObj = {};
+            const albumTitle = $(album).find(".album-name");
+
+            albumTitle[0] &&
+              Object.assign(albumObj, { albumTitle: albumTitle[0].innerText });
+            albumsArr.push(albumObj);
+          });
+        artistObj.albums = albumsArr;
       });
 
     songArray.push(songObj);
